0008badd6e960f40048b9d3af86023155f9d0838
Aarohi Bhanuj Chowdhary <146480441+bhanujongit4@users.noreply.github.com>
2025-03-15 04:39:28 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,22 @@ import { ThemeProvider } from './components/ThemeContext';
 import Header from './components/Header';
 import './globals.css';
 import { ReactNode } from 'react';
-import DarkModeProvider from './components/DarkModeProvider';
 
 interface RootLayoutProps {
   children: ReactNode;
 }
-
 export const metadata = {
   title: "Debsoc NSUT",
-  description: "Debate n shi",
+  description: "Debate",
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head>
+     <head>
         <link rel="icon" href="/images/debsoc logo 2.png" type="image/png" />
       </head>
-      <body className="min-h-screen bg-white dark:bg-black transition-colors duration-300">
-        <DarkModeProvider />
+       <body className="min-h-screen bg-white dark:bg-black transition-colors duration-300">
         <ThemeProvider>
           <Header />
           <main className="container mx-auto ">
